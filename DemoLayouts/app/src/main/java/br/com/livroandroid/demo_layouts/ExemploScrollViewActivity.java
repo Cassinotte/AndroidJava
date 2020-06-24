733e6e55793f4069f62c6a499f5f7d9f9ad6fb96
Eduardo Aparecido Cassinotte <ea.cassinotte@hotmail.com>
2020-06-24 13:15:53 -0300

    Scroll Inflabe

--- DemoLayouts/app/src/main/java/br/com/livroandroid/demo_layouts/ExemploScrollViewActivity.java
+++ DemoLayouts/app/src/main/java/br/com/livroandroid/demo_layouts/ExemploScrollViewActivity.java
@@ -15,6 +15,7 @@ public class ExemploScrollViewActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         setContentView(R.layout.activity_exemplo_scroll_view);
 
         LinearLayout layout = (LinearLayout) findViewById(R.id.layout1);
@@ -22,12 +23,12 @@ public class ExemploScrollViewActivity extends AppCompatActivity {
         for (int i = 0; i < 100; i++)
         {
             // Instancia o TextView inflando o arquivo de layout
-            //LayoutInflater inflater = LayoutInflater.from(this);
-            //TextView text = (TextView) inflater.inflate(R.layout.inflate_textview, layout, false);
+            LayoutInflater inflater = LayoutInflater.from(this);
+            TextView text = (TextView) inflater.inflate(R.layout.inflate_textview, layout, false);
 
             // Instancia o TextView pela API
-			TextView text = new TextView(this);
-			text.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT));
+			//TextView text = new TextView(this);
+			//text.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT));
 
             text.setText("Texto: " + i);
             layout.addView(text);
