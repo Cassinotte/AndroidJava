990958f254f033e650e1f1b072a292a0fba4fd51
Eduardo Aparecido Cassinotte <ea.cassinotte@hotmail.com>
2020-06-24 15:33:27 -0300

    View

---
+++ DemoViews/app/src/main/java/br/com/livroandroid/demo_views/ExemploTextoCoresActivity.java
@@ -0,0 +1,14 @@
+package br.com.livroandroid.demo_views;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ExemploTextoCoresActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_exemplo_texto_cores);
+    }
+}
