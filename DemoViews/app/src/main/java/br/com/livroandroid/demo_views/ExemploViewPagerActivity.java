519ba347c63a9f3fe4d3ee91226a905ce808499c
Eduardo Aparecido Cassinotte <ea.cassinotte@hotmail.com>
2020-06-29 16:58:36 -0300

    ViewPager

--- DemoViews/app/src/main/java/br/com/livroandroid/demo_views/ExemploViewPagerActivity.java
+++ DemoViews/app/src/main/java/br/com/livroandroid/demo_views/ExemploViewPagerActivity.java
@@ -1,14 +1,52 @@
 package br.com.livroandroid.demo_views;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
+@SuppressWarnings("deprecation")
 public class ExemploViewPagerActivity extends AppCompatActivity {
+
+    // Planetas
+    private int[] images = {
+            R.drawable.planeta_01_mercurio, R.drawable.planeta_02_venus,
+            R.drawable.planeta_03_terra, R.drawable.planeta_04_marte,
+            R.drawable.planeta_05_jupiter,
+            R.drawable.planeta_06_saturno,
+            R.drawable.planeta_07_urano,
+            R.drawable.planeta_08_neptuno,
+            R.drawable.planeta_09_plutao
+    };
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_exemplo_view_pager);
+
+        ViewPager g = (ViewPager) findViewById(R.id.viewPager);
+        g.setAdapter(new ImagemPagerAdapter(this,images));
+
+        g.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                Toast t = Toast.makeText(getBaseContext(), "Imagem: " + position, Toast.LENGTH_SHORT);
+
+                t.show();
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
     }
 }
--- DemoViews/app/src/main/java/br/com/livroandroid/demo_views/ImagemPagerAdapter.java
+++ DemoViews/app/src/main/java/br/com/livroandroid/demo_views/ImagemPagerAdapter.java
@@ -1,11 +1,14 @@
 package br.com.livroandroid.demo_views;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.viewpager.widget.PagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
 public class ImagemPagerAdapter extends PagerAdapter {
 
@@ -28,7 +31,18 @@ public class ImagemPagerAdapter extends PagerAdapter {
     public Object instantiateItem(@NonNull ViewGroup container, int position) {
 
         // Infla a view
+        View view = LayoutInflater.from(this.ctx).inflate(R.layout.adapter_imagem, container, false);
+        ImageView img = (ImageView) view.findViewById(R.id.img);
+        img.setImageResource(imagens[position]);
+        // Adiciona no layout ViewGroup
+        ((ViewGroup) container).addView(view);
+        return view;
 
+    }
+
+    @Override
+    public void destroyItem(@NonNull View container, int position, @NonNull Object object) {
+        ((ViewPager) container).removeView((View) object);
     }
 
     @Override
